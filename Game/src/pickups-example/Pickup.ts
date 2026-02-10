8dccab04fa2964759dfde8c175b0f70acd2f04ae
Joshua Husting <jhusting@series.ai>
2026-02-10 00:07:18 -0800

    Add small tween example to pickup for a bit more juice

--- Game/src/pickups-example/Pickup.ts
+++ Game/src/pickups-example/Pickup.ts
@@ -2,12 +2,15 @@ import { PlayerComponent } from "../player"
 import { Component, GameObject, InteractionZone } from "@series-ai/rundot-3d-engine"
 import * as THREE from "three"
 import { PickupSystem } from "./PickupSystem"
+import { Easing, TweenSystem } from "@series-ai/rundot-3d-engine/systems"
 
 export class Pickup extends Component {
     private interactionZone!: InteractionZone
     private interactionZoneObject!: GameObject
     private elapsedTime: number = 0
     private initialY: number = 0
+    private tweenScale: number = 1.0
+    private wasConsumed: boolean = false
 
     constructor() {
         super()
@@ -62,10 +65,58 @@ export class Pickup extends Component {
         if (!playerComponent) {
             return
         }
-        PickupSystem.removeActivePickup(this)
-        this.destroy()
+        if (this.wasConsumed) {
+            return
+        }
+        this.wasConsumed = true
+        this.quickPopTween()
     }
 
     private onExitZone(other: GameObject): void {
+    }
+
+    public quickPopTween(
+        popTarget: number = 1.35,
+        popDuration: number = 0.12,
+        fadeDuration: number = 0.3
+    ): void {
+        this.tweenScale = 1.0
+        this.gameObject.scale.set(this.tweenScale, this.tweenScale, this.tweenScale)
+
+        // Set up initial tween using local value
+        const popTween = TweenSystem.tween(
+            this,
+            "tweenScale",
+            popTarget,
+            popDuration,
+            (t: number) => Easing.easeOutQuad(t)
+        )
+        // Local value is read and then used to set game object scale
+        popTween.onUpdated((value: number) => {
+            this.gameObject.scale.set(value, value, value)
+        })
+
+        // Once initial pop tween is complete, repeat the process with a scale down fade tween
+        popTween.onCompleted(() => {
+            // Fade down to 0 scale
+            const fadeTween = TweenSystem.tween(
+                this,
+                "tweenScale",
+                0,
+                fadeDuration,
+                (t: number) => Easing.easeInOutQuad(t)
+            )
+
+            fadeTween.onUpdated((value: number) => {
+                this.gameObject.scale.set(value, value, value)
+            })
+
+            fadeTween.onCompleted(() => {
+                this.tweenScale = 0
+                this.gameObject.scale.set(0, 0, 0)
+                PickupSystem.removeActivePickup(this)
+                this.destroy()
+            })
+        })
     }
 }
